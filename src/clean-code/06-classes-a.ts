4f9910acf6c5428cf1f45762e4b63f4678f07c4c
Alexander <alexander.morenoc@outlook.com>
2022-09-24 22:40:17 -0500

    s3 Clean Code Clases / 25 Introduccion a clases en TypeScript

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -2,18 +2,26 @@
 
   type Gender = 'M'|'F';
 
+  // class Person {
+  //   public name: string;
+  //   public gender: Gender;
+  //   public birthdate: Date;
+
+  //   constructor(name: string, gender: Gender, birthdate: Date){
+  //     this.name = name;
+  //     this.gender = gender;
+  //     this.birthdate = birthdate;
+  //   }
+  // }
 
   class Person {
-    public name: string;
-    public gender: Gender;
-    public birthdate: Date;
-
-    constructor(name: string, gender: Gender, birthdate: Date){
-      this.name = name;
-      this.gender = gender;
-      this.birthdate = birthdate;
-    }
+    constructor(
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date,
+    ){}
   }
+
 
   const newPerson = new Person('Alexander', 'M', new Date('1992-12-22'));
 
